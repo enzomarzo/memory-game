77a4b88d6edf5f334614247598532ffd45e5fa67
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-07 20:39:50 +0000

    feat: update button component with onCLick event

--- src/components/foundation/button/Button.tsx
+++ src/components/foundation/button/Button.tsx
@@ -3,16 +3,18 @@ import classes from "./button.module.scss";
 interface IButtonProps {
   additionalCSS?: string;
   children?: React.ReactNode;
+  onClick: () => void;
   type?: "button" | "submit" | "reset";
 }
 
 function Button(props: IButtonProps) {
-  const { additionalCSS, children, type, ...buttonProps } = props;
+  const { additionalCSS, children, type, onClick, ...buttonProps } = props;
 
   return (
     <button
       className={`${classes.cmpButton} ${additionalCSS || ""}`}
       type={type || "button"}
+      onClick={onClick}
       {...buttonProps}
     >
       {children}
