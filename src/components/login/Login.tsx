a663e4da1150505e61adec7618fd59c6c4cf0e19
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-07 16:29:44 +0000

    feat: implement initial version of login functionality

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import Button from "../foundation/button/Button";
 import classes from "./login.module.scss";
 
 const Login = () => {
-  return <div> login component works! </div>;
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    localStorage.setItem("username", username);
+  };
+
+  return (
+    <div className={classes.cmpLogin}>
+      <h1 className={classes.cmpLoginTitle}>Memory Game</h1>
+      <p className={classes.cmpLoginMessage}>
+        Type your username to start playing
+      </p>
+
+      <form className={classes.cmpLoginForm} onSubmit={handleSubmit}>
+        <label className={classes.cmpLoginFormLabel} htmlFor="login">
+          Username
+        </label>
+        <input
+          className={classes.cmpLoginFormInput}
+          name="login"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <Button additionalCSS={classes.cmpLoginFormButton} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
