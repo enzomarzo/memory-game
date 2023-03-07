40537a7dd01d5e4139c72ce82ddd8717dd6965e1
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-07 17:16:49 +0000

    feat: add react router library

---
+++ src/components/error-page/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import { useRouteError } from "react-router-dom";
+
+type ErrorResponse = {
+  status: number;
+  statusText: string;
+  message?: string;
+  error: {
+    message: string;
+    stack: string;
+  };
+};
+
+export default function ErrorPage() {
+  const error = useRouteError() as ErrorResponse;
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error?.message}</i>
+      </p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './components/login/Login'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./components/login/Login";
+import ErrorPage from "./components/error-page/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    errorElement: <ErrorPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
