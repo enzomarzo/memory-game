314017408ba625b7f85cefa932dbfbf593fd4c5a
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-08 20:35:01 +0000

    feat: add board component

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,7 +1,34 @@
+import Card from "../common/card/Card";
+import useFetchPhotos from "../hooks/useFetchPhotos";
 import classes from "./board.module.scss";
+import { Photo } from "pexels";
+import { useContext, useEffect } from "react";
+import { Context } from "../context/Context";
 
 const Board = () => {
-  return <div> login component works! </div>;
+  const photos = useFetchPhotos();
+  const { state, dispatch } = useContext(Context);
+
+  if (!photos) {
+    return <div>There is no photos </div>;
+  }
+
+  return (
+    <div className={classes.board}>
+      {photos.map((photo: Photo, i) => {
+        const cardUniqueId = photo.id + i;
+        return (
+          <div key={cardUniqueId} className={classes.boardCard}>
+            <Card
+              photoId={photo.id}
+              src={photo.src.small}
+              alt={photo.alt || "pexels random image"}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Board;
