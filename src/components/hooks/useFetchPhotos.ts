9ea45e8876324b17d02de1880771b9c4a0c09d63
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-08 20:37:09 +0000

    feat: improve the query to get the photos with pexels library

--- src/components/hooks/useFetchPhotos.ts
+++ src/components/hooks/useFetchPhotos.ts
@@ -1,16 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import {
+  createClient,
+  PhotosWithTotalResults,
+  ErrorResponse,
+  Photo,
+} from "pexels";
+
+interface CustomResponse extends PhotosWithTotalResults {
+  photos: Photo[];
+}
 
 const useFetchPhotos = () => {
-  const [photos, setPhotos] = useState();
+  const [photos, setPhotos] = useState<Photo[] | null>(null);
   const API_KEY = import.meta.env.VITE_PEXEL_API_KEY;
+  const client = createClient(API_KEY);
+
+  const numberOfPairs = 8;
+  const query = "people";
+  const orientation = "square";
+  const size = "small";
 
   useEffect(() => {
-    const getPhotos = async () => {
-      await fetch("https://api.pexels.com/v1/search?query=people", {
-        headers: { Authorization: API_KEY },
-      })
-        .then((res) => res.json())
-        .then((res) => setPhotos(res));
+    const getPhotos = () => {
+      client.photos
+        .search({
+          query,
+          orientation,
+          per_page: numberOfPairs,
+          size,
+        })
+        .then((res: PhotosWithTotalResults | ErrorResponse) => {
+          const data = res as CustomResponse;
+          const doubledPhotos = [...data.photos, ...data.photos];
+          const shuffledPhotos = doubledPhotos.sort(() => Math.random() - 0.5);
+          setPhotos(shuffledPhotos);
+        })
+        .catch((err) => console.log(err));
     };
 
     getPhotos();
