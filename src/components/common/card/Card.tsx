177db5cc9af2717958d56625cea50bfba5082c97
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-08 20:42:56 +0000

    feat: enhance card component to handle the flipping of cards

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,24 +1,62 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../context/Context";
 import Button from "../../foundation/button/Button";
 import classes from "./card.module.scss";
 
 interface ICardProps {
   src: string;
   alt: string;
+  photoId: number;
 }
 
-function Card({ src, alt }: ICardProps) {
+function Card({ src, alt, photoId }: ICardProps) {
   const [isFlipped, setIsFlipped] = useState(true);
+  const { state, dispatch } = useContext(Context);
 
-  const onFlippCard = () => {
-    if (isFlipped) {
-      setIsFlipped(!isFlipped);
+  useEffect(() => {
+    const getCardIds = localStorage.getItem("cardId");
+    const savedCardIds = getCardIds && JSON.parse(getCardIds);
+    const permantedFlopped = savedCardIds?.includes(photoId);
+    if (permantedFlopped) {
+      setIsFlipped(false);
     }
+    if (state.step === 0 && !permantedFlopped) {
+      setIsFlipped(true);
+    }
+  }, [state]);
+
+  const firstFlip = () => {
+    setIsFlipped(false);
+    dispatch({ type: "SET_STATE", payload: { step: 1, isMatch: false } });
+  };
+
+  const secondFlip = (id: number) => {
+    setIsFlipped(false);
+    dispatch({ type: "SET_STATE", payload: { step: 2 } });
+    validateCards(id);
+  };
+
+  const validateCards = (id: number) => {
+    if (state.selectedCard === id) {
+      dispatch({ type: "SET_STATE", payload: { isMatch: true, step: 0 } });
+      dispatch({ type: "SET_STATE", payload: state.matchedCards.push(id) });
+      localStorage.setItem("cardId", JSON.stringify(state.matchedCards));
+    } else {
+      setTimeout(() => {
+        dispatch({ type: "SET_STATE", payload: { step: 0 } });
+      }, 1000);
+    }
+  };
+
+  const onFlippCard = (id: number) => {
+    dispatch({ type: "SET_STATE", payload: { selectedCard: id } });
+    if (state.step === 0) firstFlip();
+    if (state.step === 1) secondFlip(id);
   };
 
   return (
     <Button
-      onClick={onFlippCard}
+      onClick={() => onFlippCard(photoId)}
       additionalCSS={`${classes.card} ${isFlipped ? classes.cardFlipped : ""}`}
     >
       <div className={isFlipped ? classes.cardFront : classes.cardBack}>
