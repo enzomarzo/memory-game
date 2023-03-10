e02d9d15d1f456903bbfe1cda83588bc440e3aa3
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-10 16:19:04 +0000

    feat: add timer date formatter function

---
+++ src/utils/TimeFormatter.ts
@@ -0,0 +1,7 @@
+export const timeFormatter = (timeInSeconds: number) => {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
