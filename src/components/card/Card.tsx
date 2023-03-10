5c4b25e67cb19d8e53372c2930c51cb97d7ebbf7
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-10 16:07:49 +0000

    feat: simplify card component logic using useCard custom hook

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,68 +1,49 @@
-import { useContext, useEffect, useState } from "react";
-import { Context } from "../context/Context";
-import Button from "../foundation/button/Button";
+import { useContext } from "react";
 import classes from "./card.module.scss";
+import Button from "../foundation/button/Button";
+import { Context } from "../context/Context";
+import { useCard } from "./useCard";
 
 interface ICardProps {
   src: string;
   alt: string;
   photoId: number;
+  cardUniqueId: number;
 }
 
-function Card({ src, alt, photoId }: ICardProps) {
-  const [isFlipped, setIsFlipped] = useState(true);
+function Card({ src, alt, photoId, cardUniqueId }: ICardProps) {
   const { state, dispatch } = useContext(Context);
+  const { isFlipped, firstFlip, secondFlip } = useCard(cardUniqueId);
 
-  useEffect(() => {
-    const getCardIds = localStorage.getItem("cardId");
-    const savedCardIds = getCardIds && JSON.parse(getCardIds);
-    const cardsVisible = savedCardIds?.includes(photoId);
-    if (cardsVisible) {
-      setIsFlipped(false);
+  const onFlipCard = (id: ICardProps["photoId"]) => {
+    if (!isFlipped) return;
+
+    if (state.step === 0) {
+      dispatch({
+        type: "SET_STATE",
+        payload: { firstCardId: id }
+      });
+      firstFlip(cardUniqueId);
     }
-    if (state.step === 0 && !cardsVisible) {
-      setIsFlipped(true);
+
+    if (state.step === 1) {
+      dispatch({
+        type: "SET_STATE",
+        payload: { secondCardId: id }
+      });
+      secondFlip(cardUniqueId);
     }
-  }, [state]);
-
-  const firstFlip = () => {
-    setIsFlipped(false);
-    dispatch({ type: "SET_STATE", payload: { step: 1, isMatch: false } });
-  };
-
-  const secondFlip = (id: number) => {
-    setIsFlipped(false);
-    dispatch({ type: "SET_STATE", payload: { step: 2 } });
-    validateCards(id);
-  };
-
-  const validateCards = (id: number) => {
-    if (state.selectedCard === id) {
-      dispatch({ type: "SET_STATE", payload: { isMatch: true, step: 0 } });
-      dispatch({ type: "SET_STATE", payload: state.matchedCards.push(id) });
-      localStorage.setItem("cardId", JSON.stringify(state.matchedCards));
-    } else {
-      setTimeout(() => {
-        dispatch({ type: "SET_STATE", payload: { step: 0 } });
-      }, 1000);
-    }
-  };
-
-  const onFlippCard = (id: number) => {
-    dispatch({ type: "SET_STATE", payload: { selectedCard: id } });
-    if (state.step === 0) firstFlip();
-    if (state.step === 1) secondFlip(id);
   };
 
   return (
     <Button
-      onClick={() => onFlippCard(photoId)}
+      onClick={() => onFlipCard(photoId)}
       additionalCSS={`${classes.card} ${isFlipped ? classes.cardFlipped : ""}`}
     >
       <div className={isFlipped ? classes.cardFront : classes.cardBack}>
         <img
           className={classes.image}
-          src={isFlipped ? "./backflipped.jpg" : src}
+          src={isFlipped ? "/backflipped.jpg" : src}
           alt={isFlipped ? "backflipped card" : alt}
         />
       </div>
---
+++ src/components/card/useCard.ts
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../context/Context";
+import useDataStorage from "../hooks/useDataStorage";
+
+export const useCard = (actualCardUniqueId: number) => {
+  const [isFlipped, setIsFlipped] = useState(true);
+  const { state, dispatch } = useContext(Context);
+  const { getCards } = useDataStorage();
+
+  // set step 1: The user clicks on the first card
+  const firstFlip = (cardUniqueId: number) => {
+    if (actualCardUniqueId !== cardUniqueId) return;
+    setIsFlipped(false);
+    dispatch({
+      type: "SET_STATE",
+      payload: { step: 1, firstCardUniqueId: actualCardUniqueId },
+    });
+  };
+
+  // set step 2: The first is visible, and then the user clicks on the second card
+  const secondFlip = (cardUniqueId: number) => {
+    if (actualCardUniqueId !== cardUniqueId) return;
+    setIsFlipped(false);
+    dispatch({
+      type: "SET_STATE",
+      payload: { step: 2, secondCardUniqueId: actualCardUniqueId },
+    });
+  };
+
+  useEffect(() => {
+    const cards = getCards();
+    const showCards = cards.includes(actualCardUniqueId);
+    if (showCards) {
+      setIsFlipped(false);
+    }
+    if (state.step === 0 && !showCards) {
+      setIsFlipped(true);
+    }
+  }, [getCards]);
+
+  return {
+    isFlipped,
+    setIsFlipped,
+    firstFlip,
+    secondFlip,
+  };
+};
