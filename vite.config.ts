15923b7adc36da8f4b36595aa98330ab1fd1f515
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-15 17:49:33 +0000

    fix: handle typescript on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import typescript from "@rollup/plugin-typescript";
 import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
     // This is needed to run PEXELS with Vite (https://github.com/pexels/pexels-javascript/issues/31)
     replace({
       preventAssignment: true,
