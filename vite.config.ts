ccfaf7e8e190bfd446e6f730d98cce746e60a881
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-15 17:03:24 +0000

    feat: add library to run PEXELS with Vite in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // This is needed to run PEXELS with Vite (https://github.com/pexels/pexels-javascript/issues/31)
+    replace({
+      preventAssignment: true,
+      "process.env.API_KEY": {},
+      'require("isomorphic-fetch");': 'import "isomorphic-fetch";',
+    }),
+  ],
   css: {
     modules: {
       localsConvention: "camelCase",
